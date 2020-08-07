ed279de816a1e6ad4928023df54691b5a7e11353
alordash <54469554+alordash@users.noreply.github.com>
2020-08-07 22:25:16 +0300

    Fixed exports in index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const { distanceCalculation, distance, closest } = require("./lib/levenshtein");
-console.log(distance("best", "better"));
-let value = closest("money", ["none", "some", "much"]);
-console.log(value.closest_string);
-console.log(value.distance);
-
-let object = new distanceCalculation("best", "better");
-console.log(object.getMatrix());
-console.log(object.value);
-object.strings = ["entity", "identity"];
-console.log(object.getMatrix());
-console.log(object.value);
-
-console.log("exiting...");
+module.exports = require('./lib/levenshtein');
