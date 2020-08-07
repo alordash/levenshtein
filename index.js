285ff99a35d3919f8ea5966586c55664fdc912dd
alordash <54469554+alordash@users.noreply.github.com>
2020-08-07 22:14:47 +0300

    Can change strings in distanceCalculation

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const { distanceCalculation, distance, closest } = require("./lib/levenshtein");
-let matrixTest = new distanceCalculation("best", "better");
-console.log(matrixTest.getMatrix());
 console.log(distance("best", "better"));
-let value = closest("best", ["the good", "the bad", "the best"]);
+let value = closest("money", ["none", "some", "much"]);
 console.log(value.closest_string);
 console.log(value.distance);
+
+let object = new distanceCalculation("best", "better");
+console.log(object.getMatrix());
+console.log(object.value);
+object.strings = ["entity", "identity"];
+console.log(object.getMatrix());
+console.log(object.value);
+
 console.log("exiting...");
