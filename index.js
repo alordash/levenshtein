44861df4608461c43f8a5efbf1adec68c1a72e20
alordash <54469554+alordash@users.noreply.github.com>
2020-08-07 21:15:00 +0300

    Improving documentation

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const levenshtein = require('./lib/levenshtein');
-let distance = levenshtein.distance("polynomial", "exponential");
-console.log(distance);
+const { distanceCalculation, distance, closest } = require("./lib/levenshtein");
+let matrixTest = new distanceCalculation("best", "better");
+console.log(matrixTest.getMatrix());
+console.log(distance("best", "better"));
+let value = closest("best", ["the good", "the bad", "the best"]);
+console.log(value.closest_string);
+console.log(value.distance);
 console.log("exiting...");
