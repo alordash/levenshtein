ee3b6396442acb70e647e584bf37ab049812a6fc
alordash <54469554+alordash@users.noreply.github.com>
2020-08-07 20:06:59 +0300

    Added tests

--- spec/zero-test.spec.js
+++ spec/zero-test.spec.js
@@ -1,5 +1,14 @@
-describe('zero-test', function () {
-    it('zero', async function () {
-        expect(true).toBe(true);
+const { distance, closest } = require('../lib/levenshtein');
+
+describe('levenshtein-test', function () {
+    it('distance 0', async function () {
+        let val = distance("polynomial", "exponential");
+        expect(val).toBe(6);
+    });
+
+    it('closest 0', async function () {
+        let val = closest("test", ["teeeeest", "testtest", "tset", "tet", "est test"]);
+        expect(val.closest_string).toBe("tet");
+        expect(val.distance).toBe(1);
     });
 });
